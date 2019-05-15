efb8df14d0666384945572464de0400a7dedfd9c
Igor Brylin <brylin.i@ati.su>
2019-05-15 20:12:39 +0300

    Fix taskcompletionsource creating to allow execute continuation asynchronously. Also add void version of batch executor.

--- BatchExecutor/BatchExecutor.cs
+++ BatchExecutor/BatchExecutor.cs
@@ -13,6 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
+
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -22,152 +23,198 @@ using BatchExecutor.Extensions;
 
 namespace BatchExecutor
 {
-	public class BatchExecutor<TItem, TResult> : IBatchExecutor<TItem, TResult>
-	{
-		private readonly ConcurrentQueue<WorkItem<TItem, TResult>> _itemsQueue = new ConcurrentQueue<WorkItem<TItem, TResult>>();
-		private readonly int _batchSize;
-		private readonly int _counterZeroingThreshold;
-		private Timer _flushTimer;
-		private readonly Func<IReadOnlyList<TItem>, Task<IDictionary<TItem, TResult>>> _batchExecutor;
-		private int _counter;
-		private readonly ArrayPool<WorkItem<TItem, TResult>> _buffersPool = new ArrayPool<WorkItem<TItem, TResult>>();
-		private readonly ArrayPool<TItem> _argumentsPool = new ArrayPool<TItem>();
-		private int _disposed;
+    public class BatchExecutor<TItem, TResult> : IBatchExecutor<TItem, TResult>
+    {
+        private readonly ConcurrentQueue<WorkItem<TItem, TResult>> _itemsQueue = new ConcurrentQueue<WorkItem<TItem, TResult>>();
+        private readonly int _batchSize;
+        private readonly int _counterZeroingThreshold;
+        private Timer _flushTimer;
+        private readonly Func<IReadOnlyList<TItem>, Task<IDictionary<TItem, TResult>>> _batchExecutor;
+        private int _counter;
+        private readonly ArrayPool<WorkItem<TItem, TResult>> _buffersPool = new ArrayPool<WorkItem<TItem, TResult>>();
+        private readonly ArrayPool<TItem> _argumentsPool = new ArrayPool<TItem>();
+        private int _disposed;
+        internal bool AllowNullResult;
 
-		public BatchExecutor(int batchSize, Func<IReadOnlyList<TItem>, Task<IDictionary<TItem, TResult>>> batchExecutor, TimeSpan bufferFlushInterval)
-		{
-			_batchSize = batchSize;
-			_counterZeroingThreshold = int.MaxValue / _batchSize * _batchSize;
-			_batchExecutor = batchExecutor ?? throw new ArgumentNullException(nameof(batchExecutor));
-			_flushTimer = new Timer(BufferFlushCallback, null, bufferFlushInterval, bufferFlushInterval);
-		}
+        public BatchExecutor(int batchSize, Func<IReadOnlyList<TItem>, Task<IDictionary<TItem, TResult>>> batchExecutor, TimeSpan bufferFlushInterval)
+        {
+            _batchSize = batchSize;
+            _counterZeroingThreshold = int.MaxValue / _batchSize * _batchSize;
+            _batchExecutor = batchExecutor ?? throw new ArgumentNullException(nameof(batchExecutor));
+            _flushTimer = new Timer(BufferFlushCallback, null, bufferFlushInterval, bufferFlushInterval);
+        }
 
-		public Task<TResult> ExecAsync(TItem item)
-		{
-			CheckDisposed();
-			var tcs = new TaskCompletionSource<TResult>();
-			ExecInternal(item, (exception, result) => ProcessResponse(tcs, exception, result));
-			return tcs.Task;
-		}
+        public Task<TResult> ExecAsync(TItem item)
+        {
+            CheckDisposed();
+            var tcs = new TaskCompletionSource<TResult>(TaskCreationOptions.RunContinuationsAsynchronously);
+            ExecInternal(item, (exception, result) => ProcessResponse(tcs, exception, result));
+            return tcs.Task;
+        }
 
-		private void ExecInternal(TItem dataItem, Action<Exception, TResult> callback)
-		{
-			EnqueueItem(dataItem, callback);
+        private void ExecInternal(TItem dataItem, Action<Exception, TResult> callback)
+        {
+            EnqueueItem(dataItem, callback);
 
-			if (Interlocked.Increment(ref _counter) % _batchSize == 0)
-				FlushBuffer(_batchSize);
-		}
+            if (Interlocked.Increment(ref _counter) % _batchSize == 0)
+                FlushBuffer(_batchSize);
+        }
 
-		private void EnqueueItem(TItem dataItem, Action<Exception, TResult> callback)
-		{
-			var currentCounter = _counter;
-			if (currentCounter == _counterZeroingThreshold)
-				Interlocked.CompareExchange(ref _counter, 0, currentCounter);
-			_itemsQueue.Enqueue(new WorkItem<TItem, TResult> { DataItem = dataItem, Callback = callback });
-		}
+        private void EnqueueItem(TItem dataItem, Action<Exception, TResult> callback)
+        {
+            var currentCounter = _counter;
+            if (currentCounter == _counterZeroingThreshold)
+                Interlocked.CompareExchange(ref _counter, 0, currentCounter);
+            _itemsQueue.Enqueue(new WorkItem<TItem, TResult> { DataItem = dataItem, Callback = callback });
+        }
 
-		private void FlushBuffer(int flushSize)
-		{
-			var buffer = _buffersPool.GetOrCreate(_batchSize);
-			var i = 0;
-			while (!_itemsQueue.IsEmpty && i < flushSize)
-			{
-				// NOTE: don't move _itemsQueue.TryDequeue into while loop condition instead of _itemsQueue.IsEmpty. Otherwise, program may hangs.
-				_itemsQueue.TryDequeue(out WorkItem<TItem, TResult> item);
-				buffer[i] = item;
-				i++;
-			}
-			ExecMulti(i, buffer);
-		}
+        private void FlushBuffer(int flushSize)
+        {
+            var buffer = _buffersPool.GetOrCreate(_batchSize);
+            var i = 0;
+            while (!_itemsQueue.IsEmpty && i < flushSize)
+            {
+                // NOTE: don't move _itemsQueue.TryDequeue into while loop condition instead of _itemsQueue.IsEmpty. Otherwise, program may hangs.
+                _itemsQueue.TryDequeue(out WorkItem<TItem, TResult> item);
+                buffer[i] = item;
+                i++;
+            }
 
-		private void ExecMulti(int bufferLength, WorkItem<TItem, TResult>[] buffer)
-		{
-			var arguments = _argumentsPool.GetOrCreate(_batchSize);
-			for (var i = 0; i < bufferLength; i++)
-			{
-				arguments[i] = buffer[i].DataItem;
-			}
-			var argumentsSegment = new ArraySegment<TItem>(arguments, 0, bufferLength);
-			_batchExecutor(argumentsSegment).ContinueWith(t =>
-														 {
-															 var faulted = t.Status == TaskStatus.Faulted;
-															 for (var i = 0; i < bufferLength; i++)
-															 {
-																 var workItem = buffer[i];
-																 if (faulted)
-																 {
-																	 workItem.Callback(t.Exception.UnwrapAggregateException(), default(TResult));
-																 }
-																 else if (!t.Result.TryGetValue(workItem.DataItem, out TResult result))
-																 {
-																	 workItem.Callback(new KeyNotFoundException($"Record for {workItem.DataItem} not found."), default(TResult));
-																 }
-																 else
-																 {
-																	 workItem.Callback(null, result);
-																 }
-															 }
-															 _buffersPool.Release(buffer);
-															 _argumentsPool.Release(arguments);
-														 });
-		}
+            ExecMulti(i, buffer);
+        }
 
-		private static void ProcessResponse(TaskCompletionSource<TResult> tcs, Exception e, TResult result)
-		{
-			if (e != null)
-			{
-				tcs.SetException(e);
-			}
-			else
-			{
-				tcs.SetResult(result);
-			}
-		}
+        private void ExecMulti(int bufferLength, WorkItem<TItem, TResult>[] buffer)
+        {
+            var arguments = _argumentsPool.GetOrCreate(_batchSize);
+            for (var i = 0; i < bufferLength; i++)
+            {
+                arguments[i] = buffer[i].DataItem;
+            }
 
-		private void BufferFlushCallback(object state)
-		{
-			var batchSize = _batchSize;
-			var spinWait = new SpinWait();
-			while (true)
-			{
-				var counter = _counter;
-				var queueSize = counter % batchSize;
-				if (queueSize == 0)
-					return;
+            var argumentsSegment = new ArraySegment<TItem>(arguments, 0, bufferLength);
 
-				var newCounter = counter - queueSize;
-				if (Interlocked.CompareExchange(ref _counter, newCounter, counter) == counter)
-				{
-					FlushBuffer(queueSize);
-					return;
-				}
-				spinWait.SpinOnce();
-			}
-		}
+            _batchExecutor(argumentsSegment).ContinueWith(t =>
+                                                          {
+                                                              var faulted = t.Status == TaskStatus.Faulted;
+                                                              var results = faulted ? null : t.Result;
+                                                              for (var i = 0; i < bufferLength; i++)
+                                                              {
+                                                                  var workItem = buffer[i];
+                                                                  if (faulted)
+                                                                  {
+                                                                      workItem.Callback(t.Exception.UnwrapAggregateException(), default);
+                                                                  }
+                                                                  else if (results != null)
+                                                                  {
+                                                                      if (!results.TryGetValue(workItem.DataItem, out var result))
+                                                                      {
+                                                                          workItem.Callback(new KeyNotFoundException($"Record for {workItem.DataItem} not found."), default);
+                                                                      }
+                                                                      else
+                                                                      {
+                                                                          workItem.Callback(null, result);
+                                                                      }
+                                                                  }
+                                                                  else if (AllowNullResult)
+                                                                  {
+                                                                      workItem.Callback(null, default);
+                                                                  }
+                                                                  else
+                                                                  {
+                                                                      workItem.Callback(
+                                                                          new InvalidOperationException(
+                                                                              "Func<IReadOnlyList<TItem>, Task<IDictionary<TItem, TResult>>> batchExecutor must return a Dictionary, but returns null."),
+                                                                          default);
+                                                                  }
+                                                              }
 
-		private void CheckDisposed()
-		{
-			if (_disposed == 1)
-				throw new ObjectDisposedException(GetType().FullName);
-		}
+                                                              _buffersPool.Release(buffer);
+                                                              _argumentsPool.Release(arguments);
+                                                          });
+        }
 
-		private void Dispose(bool disposing)
-		{
-			if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 1)
-				return;
+        private static void ProcessResponse(TaskCompletionSource<TResult> tcs, Exception e, TResult result)
+        {
+            if (e != null)
+            {
+                tcs.SetException(e);
+            }
+            else
+            {
+                tcs.SetResult(result);
+            }
+        }
 
-			if (disposing)
-			{
-				_flushTimer.Dispose();
-				_flushTimer = null;
-				BufferFlushCallback(null);
-			}
-		}
+        private void BufferFlushCallback(object state)
+        {
+            var batchSize = _batchSize;
+            var spinWait = new SpinWait();
+            while (true)
+            {
+                var counter = _counter;
+                var queueSize = counter % batchSize;
+                if (queueSize == 0)
+                    return;
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-	}
+                var newCounter = counter - queueSize;
+                if (Interlocked.CompareExchange(ref _counter, newCounter, counter) == counter)
+                {
+                    FlushBuffer(queueSize);
+                    return;
+                }
+
+                spinWait.SpinOnce();
+            }
+        }
+
+        private void CheckDisposed()
+        {
+            if (_disposed == 1)
+                throw new ObjectDisposedException(GetType().FullName);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 1)
+                return;
+
+            if (disposing)
+            {
+                _flushTimer.Dispose();
+                _flushTimer = null;
+                BufferFlushCallback(null);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class BatchExecutor<TItem> : IBatchExecutor<TItem>
+    {
+        private readonly BatchExecutor<TItem, bool> _base;
+
+        public BatchExecutor(int batchSize, Func<IReadOnlyList<TItem>, Task> batchExecutor, TimeSpan bufferFlushInterval)
+        {
+            _base = new BatchExecutor<TItem, bool>(batchSize, async items =>
+                                                              {
+                                                                  await batchExecutor(items).ConfigureAwait(false);
+                                                                  return null;
+                                                              }, bufferFlushInterval) { AllowNullResult = true };
+        }
+
+
+        public Task ExecAsync(TItem item)
+        {
+            return _base.ExecAsync(item);
+        }
+
+        public void Dispose()
+        {
+            _base.Dispose();
+        }
+    }
 }
